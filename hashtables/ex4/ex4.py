1c8d22f0c8c15b2c1080e4127a9e5edaa43c7aa0
Jason Hinton <jason_hinton@att.net>
2020-11-09 23:09:50 -0500

    mvp

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for num in a:
+        # check for duplicates, positive or negative using abs
+        if cache.get(abs(num)):
+            # check for the negatives and add to table
+            if (cache.get(abs(num))+ num) == 0:
+                result.append(abs(num))
+        # otherwise add to cache
+        else:
+            cache[abs(num)] = num
 
     return result
 
